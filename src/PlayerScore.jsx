b318aad3e6fe516ea8f32365f1476e2a20f68099
Bradley Mubenga <bradley@Bradleys-MacBook-Air.local>
2021-07-28 10:34:56 +0200

    Level 3 Complete

--- src/PlayerScore.jsx
+++ src/PlayerScore.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function PlayerScore({playerScores}) {
+export default function PlayerScore({ playerScores }) {
+    playerScores.sort((score1, score2) => {
+        return score2.s - score1.s;
+    });
+
     let PlayerScoreHTML = playerScores.map((score, index) =>
         <ul className="individualScores" key={index}>
             <li>{score.n} : {score.s}</li>
